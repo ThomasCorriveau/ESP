a5c58816434fcf9ca0ada321ff370c1b9f162498
Twimpy1 <99621769+Twimpy1@users.noreply.github.com>
2022-02-13 21:00:03 -0500

    interface base

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print("yolo")
-print("petit test")
+import tkinter as tk
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, width=600, height=300)
+canvas.grid(columnspan=3, rowspan=3)
+
+# instructions
+instructions = tk.Label(root, text="Yo", font="Arial")
+instructions.grid(columnspan=3, column=0, row=1)
+
+
+root.mainloop()
