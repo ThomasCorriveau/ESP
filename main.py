cfb6f430cc3868dd1db71185af812396630b9abe
Momolito <99766109+Momolito@users.noreply.github.com>
2022-02-17 10:47:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ from tkinter import *
 from tkinter import filedialog
 import pygame
 import numpy as np
+import wave
+import sys
+import matplotlib.pyplot as plt
+
+wav= wave.open("test.wave","r")
+
+raw=wav.readframes(-1)
+raw= np.frombuffer(raw,"Int16")
+
+if wav.getnchannels()==2:
+    prinnt("Stereo files are not supported.Use Mono Files")
+    sys.exit(0)
+
+plt.title("Notre son")
+plt.plot(raw, color= "pink")
+plt.ylabel("Amplitude")
+plt.show()
 # pyaudio ?
 
 root = Tk()
